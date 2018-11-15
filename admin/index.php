505c2ada5dd8776463be2d1a44df066a084ac547
clariceabia <clariceabia8@gmail.com>
2018-11-15 13:50:31 +0300

    this is my commit

---
+++ admin/index.php
@@ -0,0 +1,106 @@
+<?php 
+session_start();
+if (empty($_SESSION['admin']) OR empty($_SESSION['type'])) {
+	header("Location: ../index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Admin Dashboard - HMS</title>
+	<link rel="stylesheet" type="text/css" href="../assets/style.css">
+	<style type="text/css">
+	.total{
+		height: 120px;
+		width: 190px;
+		border: 1px solid green;
+		margin-top: 25px;
+		margin-bottom: 25px;
+		margin-left: 150px;
+		float: left;
+		text-align: center;
+		padding-top: 20px;
+	}
+	</style>
+</head>
+<body>
+	<div class="wrapper">
+	<?php
+		include "includes/header.php";
+		include "includes/left.php";
+	 ?>
+		<div class="right">
+			<div class="total">
+				<b>Total Receptionist</b><hr>
+				<?php
+				require_once "../includes/connect.php";
+
+				$sql = "SELECT * FROM `users` WHERE `type`='Reception'";
+				$query = mysql_query($sql);
+				echo "<br><b style='color: green; font-family:Arial; font-size:35px;'>".$row = mysql_num_rows($query)."</b>"; 
+				 ?>
+			</div>
+
+			<div class="total">
+				<b>Total Doctors</b><hr>
+				<?php
+				require_once "../includes/connect.php";
+
+				$sql = "SELECT * FROM `users` WHERE `type`='NormalDoctor' OR `type`='DentalDoctor' OR `type`='WomenDoctor'";
+				
+				$query = mysql_query($sql);
+				echo "<br><b style='color:#408080; font-family:Arial; font-size:35px;'>".$row = mysql_num_rows($query)."</b>"; 
+				 ?>
+			</div>
+			
+			<div class="total">
+				<b>Total Laboratorist</b><hr>
+				<?php
+				require_once "../includes/connect.php";
+
+				$sql = "SELECT * FROM `users` WHERE `type`='Laboratory'";
+				$query = mysql_query($sql);
+				echo "<br><b style='color:#408080; font-family:Arial; font-size:35px;'>".$row = mysql_num_rows($query)."</b>"; 
+				 ?>
+			</div><br><br>
+			<div class="total">
+				<b>Total Admins</b><hr>
+				<?php
+				require_once "../includes/connect.php";
+
+				$sql = "SELECT * FROM `users` WHERE `type`='Admin'";
+				
+				$query = mysql_query($sql);
+				echo "<br><b style='color:#408080; font-family:Arial; font-size:35px;'>".$row = mysql_num_rows($query)."</b>"; 
+				 ?>
+			</div>
+
+			<div class="total">
+				<b>Total Pharmatist</b><hr>
+				<?php
+				require_once "../includes/connect.php";
+
+				$sql = "SELECT * FROM `users` WHERE `type`='Pharmacy'";
+				$query = mysql_query($sql);
+				echo "<br><b style='color:#408080; font-family:Arial; font-size:35px;'>".$row = mysql_num_rows($query)."</b>"; 
+				 ?>
+			</div>
+
+			<div class="total">
+				<b>Total Bursar</b><hr>
+				<?php
+				require_once "../includes/connect.php";
+
+				$sql = "SELECT * FROM `users` WHERE `type`='Bursar'";
+				$query = mysql_query($sql);
+				echo "<br><b style='color:#408080; font-family:Arial; font-size:35px;'>".$row = mysql_num_rows($query)."</b>"; 
+				 ?>
+			</div>
+		</div>
+		<?php 
+		include "includes/footer.php";
+		 ?>
+	</div>
+</body>
+</html>
